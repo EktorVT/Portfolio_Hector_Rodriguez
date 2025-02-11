a770e7b0c601c0ae2a24c1a2eb2d605a8c55b196
Hector <hector2001rdz@gmail.com>
2025-02-11 14:20:24 -0600

    Feat P[1] Add text to translate

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,18 @@ function App() {
   return (
     <>
       <Header />
-      <h1>Portfolio</h1>
       <section>
         <div>
-          <h1>{t("welcome")}</h1>
+          <h1>Jose Hector Rodriquez Rodriguez</h1>
+          <h2>{t("degree")}</h2>
           <img src="../src/images/perfil.jpg" alt="" />
           <p>{t("about_me")}</p>
+          <a
+            href="https://drive.google.com/file/d/1GABC7AI6Q7nZNEO7uFd-VE3E2uCv3hLH/view?usp=sharing"
+            target="_blank"
+          >
+            <button>{t("download")}</button>
+          </a>
         </div>
       </section>
       <Footer />
