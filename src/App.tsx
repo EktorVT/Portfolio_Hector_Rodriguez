3694b0fbede6e19638695c81136ce24f1f26724f
Hector <hector2001rdz@gmail.com>
2025-02-13 13:12:38 -0600

    Feat P[2] Add Profile View

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
-import { useTranslation } from "react-i18next";
 import "./i18n";
+import AboutMe from "./components/AboutMe/AboutMe";
 
 function App() {
-  const { t } = useTranslation();
-
   return (
     <>
       <Header />
       <section>
-        <div>
-          <h1>Jose Hector Rodriquez Rodriguez</h1>
-          <h2>{t("degree")}</h2>
-          <img src="../src/images/perfil.jpg" alt="" />
-          <p>{t("about_me")}</p>
-          <a
-            href="https://drive.google.com/file/d/1GABC7AI6Q7nZNEO7uFd-VE3E2uCv3hLH/view?usp=sharing"
-            target="_blank"
-          >
-            <button>{t("download")}</button>
-          </a>
-        </div>
+        <AboutMe />
       </section>
+
       <Footer />
     </>
   );
