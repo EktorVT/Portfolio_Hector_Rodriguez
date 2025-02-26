862d5acc8ad09d4cd559116c46c073a88a1ce05c
Hector <hector2001rdz@gmail.com>
2025-02-26 13:56:27 -0600

    Feat [P-6] add projects section

---
+++ src/data/projects.ts
@@ -0,0 +1,18 @@
+export interface Project {
+  name: string;
+  description: string;
+  technologies: string[];
+  image: string;
+  website?: string;
+  github?: string;
+}
+
+export const projects: Project[] = [
+  {
+    name: "project1", //translation key to display text
+    description: "project1_description", //Clave de traducción para mostrar texto
+    technologies: ["Vite", "TypeScript", "CSS"],
+    image: "/portfolio.png",
+    github: "https://github.com/EktorVT/portfolio"
+  }
+];
